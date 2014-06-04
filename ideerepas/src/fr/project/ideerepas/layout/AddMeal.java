3c0b67da1cec745cd6b8400a5b6254cd6ca7fe16
Elo <elodie.bouyer@hotmail.fr>
2014-06-04 23:31:22 +0200

    Adding picture.

--- ideerepas/src/fr/project/ideerepas/layout/AddMeal.java
+++ ideerepas/src/fr/project/ideerepas/layout/AddMeal.java
@@ -1,26 +1,102 @@
 package fr.project.ideerepas.layout;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import fr.project.ideerepas.R;
 import fr.project.ideerepas.meal.Meals;
 
 public class AddMeal extends Activity {
 
+	private static final int REQUEST_IMAGE_CAPTURE = 1;
 	private static String TAG = AddMeal.class.getName();
 	private Meals func;
+	private Uri photo;
 
+	/**
+	 * Called when the activity launched.
+	 */
+	@Override
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
+		Log.i(TAG, "Activity AddMeal créée.");
 
 		setContentView(R.layout.add_meal);
 		setTitle(getString(R.string.new_meal));
+	}
+
+	/**
+	 * Called when user click in the button to add a picture.
+	 * @param view Button to add a picture.
+	 */
+	public void addPicture(View view) {
+		// Create a file for a photo.
+		photo = Uri.fromFile(createImageFile());
+
+		// create Intent to take a picture and return control to the calling application
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, photo);
+
+		Log.i(TAG,"Path to the picture="+photo);
+		// start the image capture Intent
+		startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+	}
+
+	/**
+	 * Called to prepare picture.
+	 * @return The picture as a file format.
+	 */
+	private File createImageFile() {
+
+		File storageDir = new File(Environment.getExternalStoragePublicDirectory(
+				Environment.DIRECTORY_PICTURES), "IdeeRepas");
+
+		// Create the storage directory if it does not exist
+		if (! storageDir.exists()){
+			if (! storageDir.mkdirs()){
+				Log.i(TAG, "failed to create directory");
+				return null;
+			}
+		}
+
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		File image = new File(storageDir.getPath() + File.separator +
+				"IMG_"+ timeStamp + ".jpg");
+
+		return image;
+	}
+
+	/**
+	 * Called when the user finished to take his picture.
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode,requestCode,data);
+		Log.i(TAG,"Path to the picture="+photo);
+
+		try {
+			// *** On la met dans l'ImageView.
+			ImageView imgView = (ImageView) findViewById(R.id.picture);
+			imgView.setImageURI(photo);
+		}
+		catch(Exception e) {
+			Log.i(TAG, "onActivityResutl() "+ e.toString());
+		}
 	}
 
 	public void addNew(View view) {
@@ -28,6 +104,7 @@ public class AddMeal extends Activity {
 
 		// Field verification.
 		EditText editName = (EditText) findViewById(R.id.name_edit);
+		
 
 		if (editName.getText().toString().isEmpty()) {
 			editName.setError(this.getResources().getString(R.string.error_name));
@@ -44,7 +121,8 @@ public class AddMeal extends Activity {
 			public void onClick(DialogInterface dialog, int id) {
 				EditText editName = (EditText) findViewById(R.id.name_edit);
 				String name = editName.getText().toString();
-				func.add(name, null, -1);
+				
+				func.add(name, photo.toString(), -1);
 				Log.i(TAG, "Ajout de "+name+" dans la base de données.");
 				finish();
 				Intent intent = new Intent(getApplicationContext(), ListMeal.class);
